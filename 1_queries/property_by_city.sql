803f84cbfba32f5bc4aa75c46a3186f2696dfea4
austinkret <austinkret@gmail.com>
2021-04-28 21:34:41 +0000

    completed queries up to property by city

---
+++ 1_queries/property_by_city.sql
@@ -0,0 +1,6 @@
+SELECT properties.id AS id, title, cost_per_night, AVG(rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_id=properties.id
+GROUP BY properties.id
+HAVING city LIKE '%Vancouver' AND AVG(rating) > 4
+ORDER BY cost_per_night;
