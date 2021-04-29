0aee5170c348e63b111e9a1f968659e70f3b70ee
austinkret <austinkret@gmail.com>
2021-04-29 19:00:25 +0000

    adjusted seeds file, inplemented then and catch functions

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-const pool = require('pg');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
@@ -8,30 +16,18 @@ const pool = require('pg');
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithEmail = function(email) {
-//   let user;
-//   for (const userId in users) {
-//     user = users[userId];
-//     if (user.email.toLowerCase() === email.toLowerCase()) {
-//       break;
-//     } else {
-//       user = null;
-//     }
-//   }
-//   return Promise.resolve(user);
-// };
-
 const getUserWithEmail = function(email) {
   pool
     .query(`
-    SELECT email 
-    FROM users 
+    SELECT email
+    FROM users
     WHERE email = $1;`,
     [email])
-    .then((result) => result.rows[0])
+    .then((result) => {
+      result.rows[0];
+    })
     .catch((err) => null);
 };
-
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -42,14 +38,15 @@ exports.getUserWithEmail = getUserWithEmail;
 const getUserWithId = function(id) {
   pool
     .query(`
-    SELECT id 
-    FROM users 
+    SELECT id
+    FROM users
     WHERE id = $1;`,
     [id])
-    .then((result) => result.rows[0])
+    .then((result) => {
+      result.rows[0];
+    })
     .catch((err) => null);
 };
-
 exports.getUserWithId = getUserWithId;
 
 
@@ -58,24 +55,18 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-// const addUser =  function(user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// };
-
 const addUser = function(user) {
   pool
     .query(`
-    INSERT INTO users (name, email, password) 
+    INSERT INTO users (name, email, password)
     VALUES($1, $2, $3)
     RETURNING *;`,
     [user.name, user.email, user.password])
-    .then((result) => result.rows[0])
+    .then((result) => {
+      result.rows[0];
+    })
     .catch((err) => null);
 };
-
 exports.addUser = addUser;
 
 /// Reservations
@@ -98,18 +89,16 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-
-const getAllProperties = (options, limit = 10) => {
-  pool
-    .query(`
-  SELECT * 
-  FROM properties 
-  LIMIT $1;`,
-    [limit])
-    .then((result) => console.log('Results from this query: ', result.rows))
-    .catch((err) => console.log('An Error Occurred: ', err.message));
+ const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
-
 exports.getAllProperties = getAllProperties;
 
 
@@ -125,3 +114,132 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const properties = require('./json/properties.json');
+// const users = require('./json/users.json');
+// const { Pool } = require('pg');
+// /// Users
+
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'lightbnb'
+// });
+
+// /**
+//  * Get a single user from the database given their email.
+//  * @param {String} email The email of the user.
+//  * @return {Promise<{}>} A promise to the user.
+//  */
+// const getUserWithEmail = function(email) {
+//   pool
+//     .query(`
+//     SELECT email
+//     FROM users
+//     WHERE email = $1;`,
+//     [email])
+//     .then((result) => result.rows[0])
+//     .catch((err) => null);
+// };
+
+// exports.getUserWithEmail = getUserWithEmail;
+
+// /**
+//  * Get a single user from the database given their id.
+//  * @param {string} id The id of the user.
+//  * @return {Promise<{}>} A promise to the user.
+//  */
+// const getUserWithId = function(id) {
+//   pool
+//     .query(`
+//     SELECT id
+//     FROM users
+//     WHERE id = $1;`,
+//     [id])
+//     .then((result) => result.rows[0])
+//     .catch((err) => null);
+// };
+
+// exports.getUserWithId = getUserWithId;
+
+
+// /**
+//  * Add a new user to the database.
+//  * @param {{name: string, password: string, email: string}} user
+//  * @return {Promise<{}>} A promise to the user.
+//  */
+// const addUser = function(user) {
+//   pool
+//     .query(`
+//     INSERT INTO users (name, email, password)
+//     VALUES($1, $2, $3)
+//     RETURNING *;`,
+//     [user.name, user.email, user.password])
+//     .then((result) => result.rows[0])
+//     .catch((err) => null);
+// };
+
+// exports.addUser = addUser;
+
+// /// Reservations
+
+// /**
+//  * Get all reservations for a single user.
+//  * @param {string} guest_id The id of the user.
+//  * @return {Promise<[{}]>} A promise to the reservations.
+//  */
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+// exports.getAllReservations = getAllReservations;
+
+// /// Properties
+
+// /**
+//  * Get all properties.
+//  * @param {{}} options An object containing query options.
+//  * @param {*} limit The number of results to return.
+//  * @return {Promise<[{}]>}  A promise to the properties.
+//  */
+
+// const getAllProperties = (options, limit = 10) => {
+//   pool
+//     .query(`
+//   SELECT *
+//   FROM properties
+//   LIMIT $1;`,
+//     [limit])
+//     .then((result) => result.rows)
+//     .catch((err) => console.log('An Error Occurred: ', err.message));
+// };
+
+// exports.getAllProperties = getAllProperties;
+
+
+// /**
+//  * Add a property to the database
+//  * @param {{}} property An object containing all of the property details.
+//  * @return {Promise<{}>} A promise to the property.
+//  */
+// const addProperty = function(property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// };
+// exports.addProperty = addProperty;
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,9 +7,9 @@ VALUES ('Thanos', 'iaminevitable@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED
 ('Steve Rogers', 'iheartpeggy@snailmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUES (1, 'Paradise At Half Capacity', 'This place will always have half the rooms empty, this universe is over-populated', 'https://www.thanos-smiling.com', 'https://www.thanos-home.com', 1000, 10, 15, 20, 'Titan', 'Titan Road', 'Titan City', 'Titan Province', 'TH4 N0S', true),
-(2, 'Royal Palace of Wakanda', 'Welcome friends, to the land of Wakanda', 'https://www.black-panther-profile.com', 'https://www.black-panther-kingdom.com', 50, 5, 4, 4, 'Wakanda', 'Main Street', 'Birnin Zana', 'Central Wakanda', 'W4K 4EV', true),
-(6, 'Caps Humble Abode', 'Just a kid from Brooklyn looking to make rent in Washington', 'https://www.caps-profile.com', 'https://www.caps-home.com', 20, 0, 1, 1, 'USA', 'Pennsylvania Avenue', 'Washington DC', 'Washington DC', '20500-0003', true);
+VALUES (1, 'Paradise At Half Capacity', 'This place will always have half the rooms empty, this universe is over-populated', 'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/a/ab/Thanos_on_Titan_%28Illusioned%29.png/revision/latest?cb=20210112015744', 'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/a/ab/Thanos_on_Titan_%28Illusioned%29.png/revision/latest?cb=20210112015744', 1000, 10, 15, 20, 'Titan', 'Titan Road', 'Titan City', 'Titan Province', 'TH4 N0S', true),
+(2, 'Royal Palace of Wakanda', 'Welcome friends, to the land of Wakanda', 'https://nypost.com/wp-content/uploads/sites/2/2019/12/wakanda-usda.jpg?quality=80&strip=all', 'https://nypost.com/wp-content/uploads/sites/2/2019/12/wakanda-usda.jpg?quality=80&strip=all', 50, 5, 4, 4, 'Wakanda', 'Main Street', 'Birnin Zana', 'Central Wakanda', 'W4K 4EV', true),
+(6, 'Caps Humble Abode', 'Just a kid from Brooklyn looking to make rent in Washington', 'https://64.media.tumblr.com/0bf28c1fb844fe475ef1b2c5e623eab5/tumblr_inline_oi88psvIt51rsvnmp_540.png', 'https://64.media.tumblr.com/0bf28c1fb844fe475ef1b2c5e623eab5/tumblr_inline_oi88psvIt51rsvnmp_540.png', 20, 0, 1, 1, 'USA', 'Pennsylvania Avenue', 'Washington DC', 'Washington DC', '20500-0003', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2021-07-11', '2021-09-26', 1, 5),
